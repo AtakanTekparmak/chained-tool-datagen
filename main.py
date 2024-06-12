4903ae519824f72c1a3fec3e4d38c5732ad818a7
AtakanTekparmak <atakantekerparmak@gmail.com>
2024-06-12 17:29:35 +0200

    feat: Added function calling engine

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ import os
 from src.models.groq import GroqModel
 from src.schemas import ModelResponse
 
+from easy_fnc.function_caller import FunctionCallingEngine
+
 def main():
     # Load the .env file
     load_dotenv()
@@ -12,12 +14,21 @@ def main():
     model = GroqModel()
 
     # Chat with the model
-    user_query = "Can you send my friend (jasper3131@gmail.com) an email about the current sentiment about MMA in twitter and the stats for Sean Strickland please?"
+    user_query = "Can you get me a random city and the weather forecast for it?"
     raw_response = model.chat(user_query)
+    print(raw_response)
 
     # Create a ModelResponse object from the response
     model_response = ModelResponse.from_raw_response(raw_response=raw_response)
     print(model_response)
+
+    # Create a FunctionCallingEngine instance
+    fnc_engine = FunctionCallingEngine()
+    fnc_engine.add_user_functions("static/sample_functions.py")
+
+    # Call the functions from the model response
+    outputs = fnc_engine.call_functions(model_response.function_calls)
+    print(outputs)
 
 if __name__ == "__main__":
     main()
