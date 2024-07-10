2dc08943c4f8433eb6f8084b1cb9460db3a78788
AtakanTekparmak <atakantekerparmak@gmail.com>
2024-07-10 11:31:18 +0200

    fix: Small cleanup

--- src/utils.py
+++ src/utils.py
@@ -6,11 +6,11 @@ from jinja2 import Environment, FileSystemLoader
 from src.schemas import FunctionMetadata
 
 # Declare constants
-CHAINED_FNC_PROMPT_PATH = "static/chained_fnc_call.toml"
+STATIC_DIR = "static"
 FNC_TEMPLATE_PATH = "chained_fnc_call.j2"
 
 # Set up Jinja2 environment
-env = Environment(loader=FileSystemLoader(join("static", "templates")))
+env = Environment(loader=FileSystemLoader(join(STATIC_DIR, "templates")))
 
 def load_toml(file_path: str) -> dict[str, any]:
     """Load a TOML file and return the parsed content"""
@@ -28,17 +28,8 @@ def load_fnc_template(
     """Load the function template from the Jinja2 template file"""
     template = env.get_template(template_name)
     
-    if fnc_metadata:
+    if fnc_metadata is not None:
         return template.render(functions_metadata=fnc_metadata)
     else:
         print("No function metadata provided. Rendering an empty template.")
         return template.render(functions_metadata=[])
-    
-def load_chained_fnc_prompt() -> str:
-    """Load the chained function prompt from the TOML file"""
-    chained_fnc_prompt = load_toml(CHAINED_FNC_PROMPT_PATH)
-    try:
-        return chained_fnc_prompt["SYSTEM_PROMPT"]
-    except KeyError:
-        print(f"SYSTEM_PROMPT not found in TOML file: {CHAINED_FNC_PROMPT_PATH}")
-        return ""
