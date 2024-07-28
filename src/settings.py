a3c1828422a8d1e974fbf8b0cda1c76b720f0d6f
AtakanTekparmak <atakantekerparmak@gmail.com>
2024-07-28 17:09:05 +0200

    feat: Laid the groundwork for user query generation

--- src/settings.py
+++ src/settings.py
@@ -7,6 +7,7 @@ CURRICULUM_PATH = os.environ.get("CURRICULUM_PATH", os.path.join(STATIC_DIR, "cu
 # Template paths
 FN_CALL_TEMPLATE_PATH = os.environ.get("FN_CALL_TEMPLATE_PATH", "chained_fnc_call.j2")
 FN_GENERATE_TEMPLATE_PATH = os.environ.get("FN_GENERATE_TEMPLATE_PATH", "function_schema_generate.j2")
+QUERY_GENERATE_TEMPLATE_PATH = os.environ.get("QUERY_GENERATE_TEMPLATE_PATH", "user_query_generate.j2")
 
 # Result directory and paths
 RESULT_DIR = os.environ.get("RESULT_DIR", "results")
--- src/templates.py
+++ src/templates.py
@@ -2,8 +2,8 @@ from os.path import join
 
 from jinja2 import Environment, FileSystemLoader
 
-from src.schemas import FunctionsMetadata
-from src.settings import STATIC_DIR, FN_CALL_TEMPLATE_PATH, FN_GENERATE_TEMPLATE_PATH
+from src.schemas import FunctionsMetadata, FunctionSchema
+from src.settings import STATIC_DIR, FN_CALL_TEMPLATE_PATH, FN_GENERATE_TEMPLATE_PATH, QUERY_GENERATE_TEMPLATE_PATH
 
 # Set up Jinja2 environment
 env = Environment(loader=FileSystemLoader(join(STATIC_DIR, "templates")))
@@ -29,4 +29,13 @@ def load_fn_generate_template(
     ) -> str:
     """Load the function generation template from the Jinja2 template file"""
     context = {"category": category, "subcategory": subcategory, "tasks": tasks}
+    return load_jinja_template(template_name, context)
+
+def load_query_generate_template(
+        template_name: str = QUERY_GENERATE_TEMPLATE_PATH,
+        schemas: list[FunctionSchema] = [],
+        num_examples: int = 10
+    ) -> str:
+    """Load the query generation template from the Jinja2 template file"""
+    context = {"schemas": schemas, "num_examples": num_examples}
     return load_jinja_template(template_name, context)
